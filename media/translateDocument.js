e71d56b40a870f6867a803431fd3df4e642d201d
e-ozcelik <e.ozcelik@lodecom.fr>
2021-01-31 22:21:24 +0100

    WIP: Implemented basic document content replication in custom editor using the example project.

---
+++ media/translateDocument.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+// Script run within the webview itself.
+(function () {
+
+	// Get a reference to the VS Code webview api.
+	// We use this API to post messages back to our extension.
+
+	// @ts-ignore
+	const vscode = acquireVsCodeApi();
+
+	const editorContainer = /** @type {HTMLElement} */ (document.querySelector('.editor-body'));
+	/**
+	 * Render the document in the webview.
+	 */
+	function updateContent(/** @type {string} */ text) {
+            editorContainer.innerText = text;
+	}
+
+	// Handle messages sent from the extension to the webview
+	window.addEventListener('message', event => {
+		const message = event.data; // The json data that the extension sent
+		switch (message.type) {
+			case 'update':
+				const text = message.text;
+
+				// Update our webview's content
+				updateContent(text);
+
+				// Then persist state information.
+				// This state is returned in the call to `vscode.getState` below when a webview is reloaded.
+				vscode.setState({ text });
+
+				return;
+		}
+	});
+
+	// Webviews are normally torn down when not visible and re-created when they become visible again.
+	// State lets us save information across these re-loads
+	const state = vscode.getState();
+	if (state) {
+		updateContent(state.text);
+	}
+}());
